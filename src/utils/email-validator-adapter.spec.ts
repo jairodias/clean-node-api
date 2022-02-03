c60c4f3732e738cf5b0e6b4d8537c5342ee359d6
jairodias <jairopereira_dias@hotmail.com>
2022-02-03 07:36:50 -0300

    refactor: rename file

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,4 +1,4 @@
-import { EmailValidatorAdapter } from './email-validator'
+import { EmailValidatorAdapter } from './email-validator-adapter'
 
 describe('EmailValidator Adapter', () => {
   test('Should return false if validator returns false', () => {
