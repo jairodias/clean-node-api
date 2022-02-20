de10f8062d0653aae106c68fcb12cf5ddc46f5a2
jairodias <jairopereira_dias@hotmail.com>
2022-02-20 14:06:01 -0300

    feat: ensure LogControllerDecorator returns the same result of the controller

--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -2,7 +2,7 @@ import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BcryptAdapter } from '../../infra/criptography/bcrypt-adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
-import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email-validator-adapter'
 import { LogControllerDecorator } from '../decorators/log'
 
